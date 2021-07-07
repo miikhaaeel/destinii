28f80d1939a3dfea3253b97410677d1b9e01ccab
Miikhaaeel <fadowomichael1@gmail.com>
2021-07-07 23:22:26 +0100

    modification

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:destinii/story_brain.dart';
 import 'package:flutter/material.dart';
 
-//TODO: Step 15 - Run the app and see if you can see the screen update with the first story. Delete this TODO if it looks as you expected.
+
 
 void main() => runApp(Destini());
 
